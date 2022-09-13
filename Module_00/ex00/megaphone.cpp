2dc25a5a6750a5aea5efc7d533db597bb6b3b97f
Abderrahmane Chahdane <a.chahdane@gmail.com>
2022-09-13 17:41:08 +0100

    ex00 done

---
+++ Module_00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achahdan <achahdan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/13 16:49:39 by achahdan          #+#    #+#             */
+/*   Updated: 2022/09/13 17:38:09 by achahdan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	for (int i = 1; i < ac; i++)
+	{
+		std::string tmp;
+
+		tmp = av[i];
+		for (int j = 0; j < (int)tmp.length() ; j++)
+			std::cout << (char)toupper(tmp[j]);
+		if (av[i + 1])
+			std::cout << "fds";
+		else
+			std::cout << std::endl;
+	}
+	return (0);
+}
