a9513f089ec27610303bf50243468aa5fcef1e64
Abderrahmane Chahdane <a.chahdane@gmail.com>
2022-09-16 21:10:20 +0100

    M01/ex06 finished

---
+++ Module_01/ex06/Harl.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achahdan <achahdan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/16 19:42:39 by achahdan          #+#    #+#             */
+/*   Updated: 2022/09/16 21:08:39 by achahdan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+#include <algorithm>
+#include<iostream>
+
+void Harl::debug( void )
+{
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info( void )
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t putenough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning( void )
+{
+    std::cout << "I think I deserve to have some extra bacon for free."<<std::endl;
+    std::cout <<  "I’ve been coming foryears whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error( void )
+{
+    std::cout << "This is unacceptable! I want to speak to the manager now" << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+    void(Harl::*fun_ptr[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    
+    int i = 0;
+
+    while (level != levels[i] && i < 4)
+        i++;
+    switch (i)
+    {
+        case 0:
+            std::cout<<"[ DEBUG ]"<<std::endl;
+            (this->*fun_ptr[i])();
+            break;
+        case 1:
+            std::cout<<"[ INFO ]"<<std::endl;
+            (this->*fun_ptr[i])();
+            break;
+        case 2:
+            std::cout<<"[ WARNING ]"<<std::endl;
+            (this->*fun_ptr[i])();
+            break;
+        case 3:
+            std::cout<<"[ ERROR ]"<<std::endl;
+            (this->*fun_ptr[i])();
+            break;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+            break;
+    }
+}
