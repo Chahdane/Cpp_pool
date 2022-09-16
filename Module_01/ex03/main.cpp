78ce7a8a3033d49dd813589fab3bf5ae5fb8af58
Abderrahmane Chahdane <a.chahdane@gmail.com>
2022-09-16 21:15:19 +0100

    cleanup

--- Module_01/ex03/main.cpp
+++ Module_01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: achahdan <achahdan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 16:51:07 by achahdan          #+#    #+#             */
-/*   Updated: 2022/09/16 14:15:32 by achahdan         ###   ########.fr       */
+/*   Updated: 2022/09/16 21:11:42 by achahdan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int main()
     {
        Weapon club = Weapon("crude spiked club");
         HumanB jim("Jim");
-        jim.setWeapon(club);
+       jim.setWeapon(club);
         jim.attack();
     club.setType("some other type of club");
         jim.attack();
