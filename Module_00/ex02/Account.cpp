26e21522732e65f50806d08149fa12961455514b
Abderrahmane Chahdane <a.chahdane@gmail.com>
2022-09-20 23:40:46 +0100

    M00 CLEANUP

--- Module_00/ex02/Account.cpp
+++ Module_00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: achahdan <achahdan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 01:19:54 by achahdan          #+#    #+#             */
-/*   Updated: 2022/09/15 04:32:11 by achahdan         ###   ########.fr       */
+/*   Updated: 2022/09/15 12:34:34 by achahdan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,76 +18,98 @@ int		Account::_totalAmount = 0;
 int		Account::_totalNbDeposits = 0;
 int		Account::_totalNbWithdrawals = 0;
 
+std::string time_stamp()
+{
+	 time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    strftime(buf, sizeof(buf), "[%Y%m%d_%I%M%S] ", &tstruct);
+
+    return buf;
+}
+
 Account::Account( int initial_deposit )
 {
-    _amount = initial_deposit;
-    _totalAmount  += initial_deposit;
-    _accountIndex = Account::_nbAccounts;
-    std::cout << "index:" << _accountIndex << ";amount;" << initial_deposit << ";created" <<std::endl;
-    Account::_nbAccounts++;
+	_amount = initial_deposit;
+	_nbWithdrawals = 0;
+	_nbDeposits = 0;
+	_totalNbWithdrawals = 0;
+	_totalAmount  += initial_deposit;
+	_accountIndex = Account::_nbAccounts;
+	std::cout << time_stamp() << "index:" << _accountIndex << ";amount:" << initial_deposit << ";created" <<std::endl;
+	Account::_nbAccounts++;
 }
 
 Account::~Account()
 {
-    
+	std::cout << time_stamp() << "index:" << _accountIndex << ";amount:" << _amount << ";closed" <<std::endl;
 }
 
 int	Account::getNbAccounts( void )
 {
-    return _nbAccounts;
+	return _nbAccounts;
 }
 
 int	Account::getTotalAmount( void )
 {
-    return _totalAmount;
+	return _totalAmount;
 }
 
 int	Account::getNbDeposits( void )
 {
-    return _totalNbDeposits;
+	return _totalNbDeposits;
 }
 
 int	Account::getNbWithdrawals( void )
 {
-    return _totalNbWithdrawals;
+	return _totalNbWithdrawals;
 }
 
 void	Account::displayAccountsInfos( void )
 {
-    std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" <<
-        _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals<<  std::endl;
+	std::cout << time_stamp() << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" <<
+		_totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals<<  std::endl;
 }
 
 void	Account::makeDeposit( int deposit )
 {
-    _amount += deposit;
-    _totalAmount += deposit;
-    _totalNbDeposits++; 
+	int p_amount = _amount;
+	_amount += deposit;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+	_nbDeposits++;
+	std::cout << time_stamp() << "index:" << _accountIndex << ";p_amount:"<< p_amount << ";deposit:" 
+	<< deposit << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits <<std::endl;
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
-    if (withdrawal == 1)
-        return true;
-    return false;
+	int p_amount = _amount;
+	if (p_amount < withdrawal)
+	{
+		std::cout << time_stamp() << "index:" << _accountIndex << ";p_amount:"<< p_amount << ";withdrawal:refused" <<std::endl;
+		return false;
+	}
+	else
+	{
+		_nbWithdrawals++;
+		_amount -= withdrawal;
+		_totalNbWithdrawals++;
+		_totalAmount-=withdrawal;
+		std::cout << time_stamp() << "index:" << _accountIndex << ";p_amount:"<< p_amount << ";withdrawal:" 
+		<< withdrawal << ";amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals <<std::endl;
+		return true;
+	}
 }
 
 int		Account::checkAmount( void ) const
 {
-    return _amount;
+	return _amount;
 }
 
 void	Account::displayStatus( void ) const
 {
-    std::cout << "index :" << _accountIndex << ";amount:" << checkAmount() <<  std::endl;
-    
+	std::cout << time_stamp() << "index:" << _accountIndex << ";amount:"<< _amount << ";deposits:" 
+	<< _nbDeposits << ";withdrawals::" << _nbWithdrawals <<std::endl;
 }
-
-
-// display account info : [19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
-
-// display status : [19920104_091532] index:0;amount:42;deposits:0;withdrawals:0
-
-// make deposit : [19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
-
-// make withdrawal : [19920104_091532] index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1
